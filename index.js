061b00660e832eba16e7945b15eb7db0cfbcd49f
shakira <shaharris11@yahoo.com>
2023-11-27 06:41:57 -0500

    index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+// Unit Test
+// Multiplication Function
+//Create a function the multiplies two input number with an output of a product
+// Expect two interger to be numbers (3,2)
+// Expect the two number to multiply (3*2)
+// Expect the output to bt the product of the two numbers (6)
+// Expect if the wrong format is entered an error will pop up (a*2) or (two*3)
+
+
+// concatOdds function
+//Create a function the takes two arrays of integers and the output is the odd numbers
+// Expect the two array of integers as arguements
+// Expect a for loop to .push the old numbers out
+// Expect the loop to only push the old number out one time no repeats
+// Exoect the output to be a list of odd numbers
+
+
+//Functional Test
+// When a user clicks on check out and not sighned in a pop-up of sign in to account/ create account or check out as guest
+// If user clicks on check out as guest it should led to a srceen to input all the information need to check out
+// If the user clicks on the checkout with an empty cart an empty cart window should pop up
+// If the user clickes on login to account a log window should pop up 
+// If the user dosent input info and clicks the login button and erros should pop up
+// If the user puts an invald user name or password an error should pop up
+// IF user forgot password, a new password link should pop up
+// If user put in an old password while creating a new one an error and rhe reason why should pop up
+// If the user logs in with a full cart, the cart list should pull into the account once logged in.
+// If the user creates an account with a full cart, the cart list should pull into that account once its made
+// Once the final bill is total any discounts or coupon will be added 
+// all card information should all be secrued
